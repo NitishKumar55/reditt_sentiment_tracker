5aa4a78c1c084de822028ed449d7bfb255d6f77f
Nitish Kumar <nitish.k0596@gmail.com>
2026-05-16 05:09:23 +0530

    code_build

--- infra/run_flink.sh
+++ infra/run_flink.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 set -e
 
-echo "Downloading Flink code from S3..."
+# Download the CORRECT Kinesis connector (DataStream API version)
+KINESIS_JAR="/usr/lib/flink/lib/flink-connector-kinesis-1.18.1.jar"
+if [ ! -f "$KINESIS_JAR" ]; then
+    echo "Downloading Kinesis connector JAR..."
+    sudo wget -O $KINESIS_JAR \
+        https://repo.maven.apache.org/maven2/org/apache/flink/flink-connector-kinesis/1.18.1/flink-connector-kinesis-1.18.1.jar
+fi
+
+# Install Python deps
+sudo pip3 install boto3 --quiet
+
+# Pull latest Flink code from S3
 aws s3 cp s3://nitish-wiki-monitor-code/flink_job/wiki_monitor.py /home/hadoop/wiki_monitor.py
 
-echo "Installing Python deps..."
-sudo pip3 install boto3 apache-flink==1.18.1
-
-echo "Downloading Kinesis connector JAR..."
-sudo wget -O /usr/lib/flink/lib/flink-sql-connector-kinesis-1.18.1.jar \
-  https://repo.maven.apache.org/maven2/org/apache/flink/flink-sql-connector-kinesis/1.18.1/flink-sql-connector-kinesis-1.18.1.jar || true
-
-echo "Submitting Flink job..."
-flink run -py /home/hadoop/wiki_monitor.py
-
-echo "Done."
+# Run Flink with JAR explicitly specified
+flink run \
+    --jarfile $KINESIS_JAR \
+    -py /home/hadoop/wiki_monitor.py
